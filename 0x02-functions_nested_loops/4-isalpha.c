af1f0241263da09e3868e6d7629cd155a0a416c8
Olivercheru254 <chruiyotoliver25@gmail.com>
2022-09-15 13:50:19 -0700

    Task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: single letter input
+ * return: 1 functional, 0 not func
+ */
+int _isalpha(int c)
+{
+if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+return (1);
+else
+return (0);
+}
